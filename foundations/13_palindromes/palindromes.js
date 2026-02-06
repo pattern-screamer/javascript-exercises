f2a960274326def677fd50e87d8fcc1bcdbf8d2e
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-06 18:34:56 +0100

    Add description of two possible palindrome solutions

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  return string.split().toReversed().join('');
+  // return string === string.split().toReversed().join('');
 };
 
 // Do not edit below this line
