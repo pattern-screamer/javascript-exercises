3684890b19dfe39152c0aa276bf3f6dee8036b75
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-09 13:17:32 +0100

    Do foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  if (index < 0) {
+    return "OOPS";
+  }
 
+  const fibonacciSequence = [0, 1];
+  for (let i = 1; i < index; i++) {
+    fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i]);
+  }
+  return fibonacciSequence[index];
 };
 
 // Do not edit below this line
